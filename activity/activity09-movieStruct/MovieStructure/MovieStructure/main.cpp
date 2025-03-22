6e59c2133b85341c593779581b2603a8ce81d40f
Chhiring D. Lama <chhiringedu@gmail.com>
2025-03-21 23:12:29 -0700

    so far a functioning user input validation

--- activity/activity09-movieStruct/MovieStructure/MovieStructure/main.cpp
+++ activity/activity09-movieStruct/MovieStructure/MovieStructure/main.cpp
@@ -28,21 +28,57 @@ ostream& operator << (ostream& stream, const Movie& movie){
     return stream ;
 }
 
+bool isLeapYear(int year){
+    if ((year % 4 == 0 && year % 100 !=0) || year % 400 == 0){
+        return true;
+    }
+    return false;
+}
+
+bool checkInvalidDate(int day, int month, int year){
+    bool dayInValid = false, monthInValid = false, yearInValid = false;
+    
+    //invalid
+    if(year < 999 || year > 10000){
+        yearInValid = true;
+    }
+    if(month <= 0 || month > 12){
+        monthInValid = true;
+    }
+    if((day <= 0 || day > 31) || ((month == 4 || month == 6 || month == 9 || month == 11 ) && day > 30) || ((month == 2 && isLeapYear(year)) && day > 29) || (month == 2 && !isLeapYear(year) && day > 28)){
+        dayInValid = true;
+    }
+
+    if (!yearInValid && !monthInValid && !dayInValid) {
+        return true;
+    } else {
+        return false;
+    }
+
+    /*
+     if day has to be < 1 or > 31
+     if month is april(04), june(06) ,september(09) , and novmeber(11) then day has to be <= 30
+     if month == 2 and its a leap year then day has to be less than 29
+     */
+}
+
 //movie function
 Movie createMovie(){
     string name;
-    int day = 0, month = 0, year = 0;
-    double rating = 0;
+    int day, month, year;
+    double rating;
     string director;
-    int noOfDirector = 0;
+    int noOfDirector;
     vector<string> directorList;
-    bool validDate = false;
+//    bool validDate = false;
     
     cout << "Enter name of the movie: ";
     getline(cin, name);
+    
+    
     do {
         //ask user for input
-        cout << "Release date (00/00/0000)" << endl;
+        cout << "Release date (dd/mm/yyyy)" << endl;
         cout << "Day: "; //no number >= 0 or < 31?
         cin >> day;
         cout << "Month: "; //no number >= 0 or < 12
@@ -51,10 +87,19 @@ Movie createMovie(){
         //if year is leap year and month is 02 || 2 then allow user to input 29 as day else return error
         cin >> year;
         //check if input is valid, if not run loop again
-    } while (!validDate);
+        if(!checkInvalidDate(day, month, year)){
+            cout << "Invalid date. Try again!" << endl;
+        }
+    } while(!checkInvalidDate(day, month, year));
+    
+    do {
+        cout << "Current Rating (0/10): "; //no number > 0 or < 10
+        cin >> rating;
+        if(rating < 0 || rating > 10){
+            cout << "Invalid rating. Try again!" << endl;
+        }
+    } while (rating < 0 || rating > 10);
 
-    cout << "Current Rating (0/10): "; //no number > 0 or < 10
-    cin >> rating;
     cout << "Director(s)" << endl;
     cout << "Please enter the number of directors involved in this movie: ";
     cin >> noOfDirector;
